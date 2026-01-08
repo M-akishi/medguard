efb3e34977479ba90443333346a0dcb1841befea
M-akishi <camposmaximiliano12@gmail.com>
2026-01-08 11:41:59 -0300

    tarea: iniciar proyecto android studio

---
+++ app/src/main/java/dev/medguard/app/MainActivity.kt
@@ -0,0 +1,30 @@
+package dev.medguard.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.medguard.app.ui.theme.MedGuardTheme
+import dev.medguard.app.ui.App
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        setContent {
+            MedGuardTheme {
+                App()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/dev/medguard/app/ui/app.kt
@@ -0,0 +1,8 @@
+package dev.medguard.app.ui
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun App() {
+    Text(text = "MedGuard")
+}
